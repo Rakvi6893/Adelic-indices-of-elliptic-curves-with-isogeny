026277daf9a4fb750b5308c2354b2443bda080ed
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-11-25 21:27:45 -0500

    Update 5 isogeny l adic images.m

--- 5 isogeny l adic images.m
+++ 5 isogeny l adic images.m
@@ -62,15 +62,6 @@ PointsViaLifting(Model,7,2); // empty
 // Considering 11-adic images and p-adic images for 19<= p < 37
 // Solved via Proposition 22
 
-//Considering 13-adic images
-// 13.91.3.a.1
-load "OpenImage-master/main/FindOpenImage.m";  // please change the path according to local directory on your system
-
-j:=2^(18)*3^3*13^4*127^3*139^3*157^3*283^3*929/(5^(13)*61^(13));
-E:=EllipticCurveFromjInvariant(j);
- G:=FindOpenImage(E); 
-pi:=hom<GL(2,Integers(864370))->GL(2,Integers(5))|[GL(2,Integers(5))!GL(2,Integers(864370)).i:i in [1..#Generators(GL(2,Integers(864370)))]]>;
-Order(GL(2,Integers(5)))/Order(pi(G)); // 1
 
 
 //Considering 17-adic and 37-adic images
