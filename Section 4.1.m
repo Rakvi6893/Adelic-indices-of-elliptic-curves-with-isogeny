23378db122ee3e78152470f7f6277cf2b0170515
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-11-25 22:04:55 -0500

    Update Section 4.1.m

--- Section 4.1.m
+++ Section 4.1.m
@@ -6,6 +6,8 @@ load "OpenImage-master/main/FindOpenImage.m";  // please change the path accordi
 j:=2^(18)*3^3*13^4*127^3*139^3*157^3*283^3*929/(5^(13)*61^(13));
 E:=EllipticCurveFromjInvariant(j);
  G:=FindOpenImage(E); 
+ Factorization(864370); //adelic level
+//[ <2, 1>, <5, 1>, <13, 1>, <61, 1>, <109, 1> ]
 pi:=hom<GL(2,Integers(864370))->GL(2,Integers(2))|[GL(2,Integers(2))!GL(2,Integers(864370)).i:i in [1..#Generators(GL(2,Integers(864370)))]]>;
 Order(GL(2,Integers(2)))/Order(pi(G)); // 1
 
