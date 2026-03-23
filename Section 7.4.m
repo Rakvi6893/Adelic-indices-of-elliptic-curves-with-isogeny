a6cc2bd17a6c02c57c62036417d430ddd2c40d12
Jacob Mayle <61399623+maylejacobj@users.noreply.github.com>
2026-03-23 14:24:06 -0400

    Update Section 7.4.m

--- Section 7.4.m
+++ Section 7.4.m
@@ -1,14 +1,19 @@
-//312.28.1.a.1
+load "OpenImage-master/main/GL2GroupTheory.m"; // Modify the path as needed
+load "OpenImage-master/main/ModularCurves.m"; // Modify the path as needed
 
-x:=AssociativeArray();
+//312.28.1.a.1
+N := 312;
+gens := [[20, 71, 75, 76], [35, 306, 7, 139], [223, 203, 61, 120], [226, 23, 1, 256], [259, 39, 271, 218]];
+gens := [ Eltseq(Transpose(Matrix(2,2,g))) : g in gens];
+X:=CreateModularCurveRec(N,gens);
+C:=FindModelOfXG(X,80)`psi;
+C;
 P:=ProjectiveSpace(Rationals(),2);
 for i in [1..3] do;
-x[i]:=P.i;
+	x[i]:=P.i;
 end for;
-C:=Curve(P,[
--6*x[1]^3 - 36*x[1]^2*x[2] - 78*x[1]*x[2]^2 + x[2]*x[3]^2
-]); // Model taken from Zywina's github repo
-Pts:=PointSearch(C,100);
+C:=Curve(P, C);
+Pts:=PointSearch(C,1000);
 E:=EllipticCurve(C,Pts[1]);
-Rank(E); //0 true
-TorsionSubgroup(E); // Z/2Z 
+Rank(E);//0
+TorsionSubgroup(E);//2
