9c23614b57c45ecf397c6118baa3e2e08da6ebf4
Jacob Mayle <61399623+maylejacobj@users.noreply.github.com>
2026-03-24 07:58:09 -0400

    Update Section 7.4.m

--- Section 7.4.m
+++ Section 7.4.m
@@ -1,13 +1,16 @@
-load "OpenImage-master/main/GL2GroupTheory.m"; // Modify the path as needed
-load "OpenImage-master/main/ModularCurves.m"; // Modify the path as needed
+ChangeDirectory("Modular-main"); // Modify the path as needed
+AttachSpec("Modular.spec");
 
 //312.28.1.a.1
 N := 312;
 gens := [[20, 71, 75, 76], [35, 306, 7, 139], [223, 203, 61, 120], [226, 23, 1, 256], [259, 39, 271, 218]];
 gens := [ Eltseq(Transpose(Matrix(2,2,g))) : g in gens];
 X:=CreateModularCurveRec(N,gens);
-C:=FindModelOfXG(X,80)`psi;
+C:=FindModelOfXG(X)`psi;
 C;
+// [
+//    78*x[1]^2*x[2] - 36*x[1]*x[2]^2 + x[1]*x[3]^2 + 6*x[2]^3
+// ]
 P:=ProjectiveSpace(Rationals(),2);
 C:=Curve(P, C);
 Pts:=PointSearch(C,1000);
